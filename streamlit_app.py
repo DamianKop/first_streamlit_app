7aaad2a75685aaed706a004feff58f037d4333bf
DamianKop <152287742+DamianKop@users.noreply.github.com>
2023-12-04 16:53:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')");
